43f78140fcd7354e5dda4704aefb919bd8b4e728
Hansen <hdan2580@gmail.com>
2022-10-15 11:35:07 -0700

    shared object and common code

---
+++ src/common/sharedobject.py
@@ -0,0 +1,38 @@
+from threading import Lock
+
+from wpqueue import WaypointQueue
+
+class SharedObject():
+    def __init__(self):
+        self._newmission = WaypointQueue()
+        self._newmission_lk = Lock()
+        self._newmission_flag = False
+        self._newmission_flag_lk = Lock()
+    
+    def gcom_newmission_flagcheck(self):
+        return self._newmission_flag
+    
+    def gcom_newmission_set(self, wpq):
+        self._newmission_flag_lk.acquire()
+        self._newmission_flag = True
+        
+        self._newmission_lk.acquire()
+        self._newmission = wpq
+        
+        self._newmission_lk.release()
+        self._newmission_flag_lk.release()
+    
+    def mps_newmission_get(self): 
+        if self._newmission_flag:
+            self._newmission_flag_lk.acquire()
+            self._newmission_flag = False
+
+            self._newmission_lk.acquire()
+            ret = self._newmission
+            self._newmission = WaypointQueue() #TODO check mutability
+            
+            self._newmission_lk.release()
+            self._newmission_flag_lk.release()
+            return ret
+        else:
+            return None
