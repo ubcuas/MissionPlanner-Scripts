a56db3bb68f29bf544e2dbf114d9740e763439cb
Hansen <hdan2580@gmail.com>
2022-10-29 13:39:39 -0700

    implemented more endpoints

--- src/mps_server/server.py
+++ src/mps_server/server.py
@@ -39,37 +39,72 @@ class MPS_Handler(socketserver.BaseRequestHandler):
         current_lat = str(parameters[0]).strip(' b\'')
         current_lng = str(parameters[1]).strip(' b\'')
         current_alt = str(parameters[2]).strip(' b\'')
+        current_hdg = str(parameters[3]).strip(' b\'')
+        current_vel = str(parameters[4]).strip(' b\'')
 
         print(f"Current Location: lat: {current_lat} lng: {current_lng} alt: {current_alt}")
+        print(f"                  hdg: {current_hdg} vel: {current_vel}")
         current_wp = Waypoint(current_lat, current_lng, current_alt)
 
-        #check if current mission has completed
-        if (self.server._current_mission.mission_complete()):
-            socket.sendto(bytes("IDLE 0 0 0", "utf-8"), self.client_address)
-            print("Idling...")
+        #updated shared obj with location data
+        self.server._so.mps_status_set({"velocity":current_vel, "latitude":current_lat, "longitude":current_lng, "altitude":current_alt, "heading":current_hdg})
+
+        #send instruction to UAV
+        socket.sendto(bytes(self.next_instruction(), "utf-8"), self.client_address)
+    
+    def next_instruction(self):
+        instruction = "IDLE 0 0 0" #default instr is to idle
+
+        #check if we should lock
+        if self.server._so.mps_locked_get():
+            if self.server._locked:
+                #still locked, idle
+                pass
+            else:
+                #tell the UAV to remove the current waypoint
+                instruction = "LOCK 1 0 0"
+                self.server._locked = True
+        else:
+            #reset _locked if coming out of locked state
+            if self.server._locked:
+                self.server._locked = False
+
             #check for a new mission
             nextwpq = self.server._so.mps_newmission_get()
-            if nextwpq != None:
+            if nextwpq != None: 
+                #overwrite the current mission with the new one
                 print("New Mission Found!")
-                self.server._current_mission = Mission(nextwpq) #TODO overwrite the queue instead
-        else:
-            #check progress
-            if (self.server._current_mission.mission_check_wp(current_wp)):
-                print("Waypoint Reached!")
-
-            #send waypoint to UAV
-            if (self.server._current_mission.mission_complete()):
-                print("Mission Complete!")
-                socket.sendto(bytes("IDLE 0 0 0", "utf-8"), self.client_address)
+                self.server._current_mission = Mission(nextwpq)
+                #IDLE instruction
             else:
-                #send the next waypoint to the UAV
-                socket.sendto(bytes("NEXT " + str(self.server._current_mission.mission_current_wp()), "utf-8"), self.client_address)
+                #check if current mission has completed
+                if (self.server._current_mission.mission_complete()):
+                    print("Idling...")
+                    #IDLE instruction
+                else:
+                    #check progress
+                    if (self.server._current_mission.mission_check_wp(current_wp)):
+                        print("Waypoint Reached!")
+                        #IDLE instruction
+
+                    #send waypoint to UAV
+                    if (self.server._current_mission.mission_complete()):
+                        print("Mission Complete!")
+                        #IDLE instruction
+                    else:
+                        #send the next waypoint to the UAV
+                        instruction = "NEXT " + str(self.server._current_mission.mission_current_wp())
+        
+        return instruction
+            
 
 class MPS_Internal_Server(socketserver.UDPServer):
     def __init__(self, hptuple, handler, so):
         self._so = so
 
         self._current_mission = Mission() #empty mission
+
+        self._locked = False
 
         #superclass constructor
         super().__init__(hptuple, handler)
