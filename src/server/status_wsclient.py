1c8a17d078e855a4639a1816e13cbde9c0e64d3a
Hansen <hdan2580@gmail.com>
2024-05-16 21:09:43 -0700

    integration testing

--- src/server/status_wsclient.py
+++ src/server/status_wsclient.py
@@ -1,5 +1,4 @@
 import time
-import json
 import socketio
 
 from server.common.sharedobject import SharedObject
@@ -12,16 +11,13 @@ class Status_Client():
     def __init__(self, shared_obj: SharedObject):
         self._so: SharedObject = shared_obj
         self._url: str = ""
-
-    def get_status_json(self) -> str:
-        return json.dumps(self._so.get_status().as_dictionary())
     
     def handle_error(self, data):
         print(f"Error sending status data: {data}")
     
     def connect_to(self, production: bool, host: str, port: int):
         print("Status Websocket Client starting...")
-        self._url = f"ws://{host}:{port}/socket.io/"
+        self._url = f"ws://{host}:{port}"
 
         self.sio = socketio.Client()
         self.sio.on('error', self.handle_error)
@@ -34,5 +30,6 @@ class Status_Client():
                 print(f"Status Websocket Client: Connection failed, retrying in {RECONNECT} second(s)")
 
         while True:
-            self.sio.emit('drone_update', self.get_status_json())
+            self.sio.emit('drone_update', self._so.get_status().as_dictionary())
             time.sleep(DELAY)
+            #A BadNamespaceError will occur when GCOM disconnects suddenly - leverage this for a reconnect?
