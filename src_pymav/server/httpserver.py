5ff55f7e577b59ddcdeaa8c1e50a388573a63b7c
wtarit <60599564+wtarit@users.noreply.github.com>
2024-11-16 15:12:26 -0800

    Implemented change_flight_mode but its not working

--- src_pymav/server/httpserver.py
+++ src_pymav/server/httpserver.py
@@ -1,19 +1,21 @@
 from flask import Flask, request
-import json
 from flask_socketio import SocketIO
+
+from pymavlink.mavutil import mavfile
 
 from server.operations.takeoff import takeoff
 from server.operations.queue import new_mission
 from server.operations.get_info import get_status
+from server.operations.change_modes import change_flight_mode
 
-from server.common.conversion import *
 from server.common.wpqueue import WaypointQueue, Waypoint
 from server.common.status import Status
 from server.common.encoders import command_string_to_int, command_int_to_string
 
-class HTTP_Server():
+
+class HTTP_Server:
     def __init__(self, mav_connection):
-        self.mav_connection = mav_connection
+        self.mav_connection: mavfile = mav_connection
 
     def serve_forever(self, production=True, HOST="localhost", PORT=9000):
         print("GCOM HTTP Server starting...")
@@ -24,15 +26,15 @@ class HTTP_Server():
         @app.route("/", methods=["GET"])
         def index():
             return "Server Running", 200
-        
+
         @app.route("/queue", methods=["GET"])
         def get_queue():
-            #TODO: notes for refactor
+            # TODO: notes for refactor
             # works by reading the entire queue, and also reading
             # the current waypoint we are on (a part of status)
             # returns only the waypoints we have left in the mission,
             # not the ones already traveled to
-            
+
             # self._so.gcom_currentmission_trigger_update()
             # while self._so._currentmission_flg_ready == False:
             #     #time.sleep(0.01)
@@ -44,7 +46,7 @@ class HTTP_Server():
             #     wp_dict = wp.get_asdict()
             #     wp_dict.update(wp.get_command())
             #     formatted.append(wp_dict)
-            
+
             # wpno = int(self._so.get_status()._wpn)
             # remaining = formatted[wpno-1:]
             # retJSON = json.dumps(remaining) # This should convert the dict to JSON
@@ -52,43 +54,53 @@ class HTTP_Server():
             # print("Queue sent to GCOM")
 
             return "", 200
-        
+
         @app.route("/queue", methods=["POST"])
         def post_queue():
             payload = request.get_json()
 
-            #ret: Status = self._so.get_status() TODO replace with status call
+            # ret: Status = self._so.get_status() TODO replace with status call
             ret = {}
             last_altitude = ret.get("alt", 50)
 
             wpq = []
             for wpdict in payload:
-                altitude = wpdict.get('altitude')
+                altitude = wpdict.get("altitude")
                 if altitude != None:
                     last_altitude = altitude
                 else:
                     altitude = last_altitude
 
-                command = wpdict.get('command', "WAYPOINT") 
+                command = wpdict.get("command", "WAYPOINT")
                 # converts any unknown waypoint types to WAYPOINT
                 command = command_int_to_string(command_string_to_int(command))
 
-                param1 = wpdict.get('param1', 0)
-                param2 = wpdict.get('param2', 0)
-                param3 = wpdict.get('param3', 0)
-                param4 = wpdict.get('param4', 0)
-                
-                wp = Waypoint(wpdict['id'], wpdict['name'], wpdict['latitude'], wpdict['longitude'], last_altitude, 
-                              command, param1, param2, param3, param4)
+                param1 = wpdict.get("param1", 0)
+                param2 = wpdict.get("param2", 0)
+                param3 = wpdict.get("param3", 0)
+                param4 = wpdict.get("param4", 0)
+
+                wp = Waypoint(
+                    wpdict["id"],
+                    wpdict["name"],
+                    wpdict["latitude"],
+                    wpdict["longitude"],
+                    last_altitude,
+                    command,
+                    param1,
+                    param2,
+                    param3,
+                    param4,
+                )
                 wpq.append(wp)
-            
+
             new_mission(self.mav_connection, WaypointQueue(wpq.copy()))
             copy = WaypointQueue(wpq.copy()).aslist()
             wpq.clear()
 
             return "ok", 200
-        
-        @app.route("/insert", methods=['POST'])
+
+        @app.route("/insert", methods=["POST"])
         def post_insert_wp():
             payload = request.get_json()
 
@@ -98,73 +110,83 @@ class HTTP_Server():
             # gets new waypoints
             new_waypoints = []
             for wpdict in payload:
-                altitude = wpdict.get('altitude')
+                altitude = wpdict.get("altitude")
                 if altitude != None:
                     last_altitude = altitude
                 else:
                     altitude = last_altitude
 
-                command = wpdict.get('command', "WAYPOINT") 
+                command = wpdict.get("command", "WAYPOINT")
                 # converts any unknown waypoint types to WAYPOINT
                 command = command_int_to_string(command_string_to_int(command))
 
-                param1 = wpdict.get('param1', 0)
-                param2 = wpdict.get('param2', 0)
-                param3 = wpdict.get('param3', 0)
-                param4 = wpdict.get('param4', 0)
-                
-                wp = Waypoint(wpdict['id'], wpdict['name'], wpdict['latitude'], wpdict['longitude'], last_altitude, 
-                              command, param1, param2, param3, param4)
+                param1 = wpdict.get("param1", 0)
+                param2 = wpdict.get("param2", 0)
+                param3 = wpdict.get("param3", 0)
+                param4 = wpdict.get("param4", 0)
+
+                wp = Waypoint(
+                    wpdict["id"],
+                    wpdict["name"],
+                    wpdict["latitude"],
+                    wpdict["longitude"],
+                    last_altitude,
+                    command,
+                    param1,
+                    param2,
+                    param3,
+                    param4,
+                )
                 new_waypoints.append(wp)
-            
+
             # insert new waypoints start at index
             new_mission(self.mav_connection, WaypointQueue(new_waypoints.copy()))
             copy = WaypointQueue(new_waypoints.copy()).aslist()
             new_waypoints.clear()
 
             return "Waypoint Inserted", 200
-        
-        @app.route("/clear", methods=['GET'])
+
+        @app.route("/clear", methods=["GET"])
         def get_clear_queue():
             new_mission(self.mav_connection, WaypointQueue([]))
 
             return "Mission has been Cleared", 200
-                
+
         @app.route("/status", methods=["GET"])
         def get_status_handler():
-            #TODO: ensure that the status has all the fields
+            # TODO: ensure that the status has all the fields
             # ret: Status = self._so.get_status()
             # retJSON = json.dumps(ret.as_dictionary())
 
             # print("Status sent to GCOM")
             s = get_status(self.mav_connection)
             return s, 200
-        
+
         @app.route("/takeoff", methods=["POST"])
         def post_takeoff():
             payload = request.get_json()
 
-            if not('altitude' in payload):
+            if not ("altitude" in payload):
                 return "Altitude cannot be null", 400
 
-            altitude = int(payload['altitude'])
+            altitude = int(payload["altitude"])
             print(f"Taking off to altitude {altitude}")
-            
+
             try:
                 result = takeoff(self.mav_connection, altitude)
             except ValueError:
                 result = 1
-            
+
             if result == 0:
                 return "Takeoff command successful", 200
             else:
                 return "Takeoff unsuccessful", 400
-        
+
         @app.route("/arm", methods=["PUT"])
         def put_arm_disarm_drone():
             input = request.get_json()
 
-            if input['arm'] in [1, 0]:
+            if input["arm"] in [1, 0]:
                 print(f"arming {int(input['arm'])}")
                 # self._so.arm_set(int(input['arm']))
 
@@ -172,23 +194,29 @@ class HTTP_Server():
                 # while result == None:
                 #     result = self._so.arm_get_result()
                 #     time.sleep(0.05)
-                result = 0 # TODO change to arm operation call
+                result = 0  # TODO change to arm operation call
                 if result == 1:
-                    return f"OK! {'Armed drone' if input['arm'] else 'Disarmed drone'}", 200
+                    return (
+                        f"OK! {'Armed drone' if input['arm'] else 'Disarmed drone'}",
+                        200,
+                    )
                 else:
-                    return f"Arm/disarm failed - drone is NOT in the requested state", 418
+                    return (
+                        f"Arm/disarm failed - drone is NOT in the requested state",
+                        418,
+                    )
             else:
                 return f"Unrecognized arm/disarm command parameter", 400
 
         @app.route("/rtl", methods=["GET", "POST"])
         def get_post_rtl():
-            altitude = request.get_json().get('altitude', 50)
+            altitude = request.get_json().get("altitude", 50)
 
             print(f"RTL at {altitude}")
-            #self._so.gcom_rtl_set(altitude) #TODO call into change modes operation
+            # self._so.gcom_rtl_set(altitude) #TODO call into change modes operation
 
             return "Returning to Launch", 200
-        
+
         @app.route("/land", methods=["GET"])
         def get_land():
             print("Landing")
@@ -199,36 +227,47 @@ class HTTP_Server():
         @app.route("/land", methods=["POST"])
         def post_land():
             land = request.get_json()
-            if not 'latitude' in land or not 'longitude' in land:
+            if not "latitude" in land or not "longitude" in land:
                 return "Latitude and Longitude cannot be null", 400
-            
-            #self._so.land_at_pos_set(land) #TODO call into landing procedure
+
+            # self._so.land_at_pos_set(land) #TODO call into landing procedure
 
             return "Landing at Specified Location", 200
-        
+
         @app.route("/home", methods=["POST"])
         def post_home():
             home = request.get_json()
 
-            if 'longitude' not in home or 'latitude' not in home or 'altitude' not in home:
+            if (
+                "longitude" not in home
+                or "latitude" not in home
+                or "altitude" not in home
+            ):
                 return "Long/lat/alt cannot be null", 400
 
             # #self._so.gcom_newhome_set(home) TODO call into set new home operation
 
             return "Setting New Home", 200
-        
+
         @app.route("/flightmode", methods=["PUT"])
         def put_flight_mode():
             input = request.get_json()
-            
-            if input['mode'] in ['loiter', 'stabilize', 'auto', 'guided']:
-                #self._so.flightmode_set(input['mode']) # TODO call into operation
+
+            # if input['mode'] in ['loiter', 'stabilize', 'auto', 'guided']:
+            # TODO: support loiter
+            if input["mode"] in ["stabilize", "auto", "guided"]:
+                change_flight_mode(
+                    self.mav_connection,
+                    self.mav_connection.target_system,
+                    self.mav_connection.target_component,
+                    input["mode"],
+                )
                 return f"OK! Changed mode: {input['mode']}", 200
-            elif input['mode'] in ['copter', 'plane']:
-                print("changing mode")
-                #self._so.flightConfig_set(input['mode']) # TODO call into operation
-                return f"OK! Changed mode: {input['mode']}", 200
+            # elif input["mode"] in ["copter", "plane"]:
+            #     print("changing mode")
+            #     # self._so.flightConfig_set(input['mode']) # TODO call into operation
+            #     return f"OK! Changed mode: {input['mode']}", 200
             else:
                 return f"Unrecognized mode: {input['mode']}", 400
-        
-        socketio.run(app, host='0.0.0.0', port=PORT, debug=True, use_reloader=False)
+
+        socketio.run(app, host="0.0.0.0", port=PORT, debug=True, use_reloader=False)
--- src_pymav/server/operations/change_modes.py
+++ src_pymav/server/operations/change_modes.py
@@ -1,5 +1,54 @@
-def change_flight_mode():
+from pymavlink.mavutil import mavfile
+
+# TODO: Should we be referring to pymavlink.dialects.v10.cubepilot here?
+from pymavlink.dialects.v10.ardupilotmega import MAVLink
+from pymavlink.dialects.v10 import ardupilotmega as dialect
+
+
+def change_flight_mode(
+    mav_connection: mavfile, tgt_sys_id: int = 1, tgt_comp_id: int = 1, flightmode: str = ""
+):
+    flightmode_lookup = {
+        # "loiter": dialect.MAV_MODE_LOITER,
+        "stabilize": dialect.MAV_MODE_STABILIZE_ARMED,
+        "auto": dialect.MAV_MODE_AUTO_ARMED,
+        "guided": dialect.MAV_MODE_GUIDED_ARMED,
+    }
+
+    link: MAVLink = mav_connection.mav
+    link.command_long_send(
+        target_system=tgt_sys_id,
+        target_component=tgt_comp_id,
+        command=dialect.MAV_CMD_DO_SET_MODE,
+        confirmation=0,
+        param1=flightmode_lookup[flightmode],
+        param2=0,
+        param3=0,
+        param4=0,
+        param5=0,
+        param6=0,
+        param7=0,
+    )
+    print(verify_ack(mav_connection, "test"))
+
+
+def change_aircraft_type(mav_connection: mavfile):
     pass
 
-def change_aircraft_type():
-    pass
+def verify_ack(mavlink_connection: mavfile, error_msg: str) -> bool:
+    """
+    Verifies the ack response.
+
+    Args:
+        master (mavutil.mavlink_connection): The MAVLink connection to use.
+        error_msg (str): The error message to log if ack verification fails.
+
+    Returns:
+        bool: True if ack verification successful, False otherwise.
+    """
+    ack = mavlink_connection.recv_match(type='COMMAND_ACK', blocking=True, timeout=3)
+    print(ack)
+    # if ack.type != 0:
+    #     print(f'{error_msg}: {ack.type}')
+    #     return False
+    return True
