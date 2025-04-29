a0b5040dbe896be86fab412d2513dc85c15db5a0
Hansen <hdan2580@gmail.com>
2025-04-28 23:01:06 -0700

    camera callback

--- src_pymav/server/httpserver.py
+++ src_pymav/server/httpserver.py
@@ -18,7 +18,7 @@ from server.utilities.request_message_streaming import set_parameter
 from server.common.wpqueue import WaypointQueue, Waypoint
 from server.common.status import Status
 from server.common.encoders import command_string_to_int, command_int_to_string
-from server.common.callback import CallbackSystem
+from server.common.callback import CallbackSystem, Callback
 
 
 class HTTP_Server:
@@ -29,6 +29,13 @@ class HTTP_Server:
         self.callback_sys = CallbackSystem(self.mav_connection, self.miscellaneous_state)
 
         # TODO Handle Camera Protocol via Callbacks?
+        self.callback_sys.register_callback(
+            Callback(
+                "Print all CAMERA_FEEDBACK messages",
+                'CAMERA_FEEDBACK',
+                only_once=False
+            )
+        )
 
     def serve_forever(self, production=True, HOST="localhost", PORT=9000):
         print("GCOM HTTP Server starting...")
